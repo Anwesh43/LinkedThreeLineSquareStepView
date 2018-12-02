7e6f1ca7c5ff859b6c6b01a4573d2e287dcf6888
Anwesh43 <anweshthecool0@gmail.com>
2018-12-02 20:35:15 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedthreelinesquarestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedthreelinesquarestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedthreelinesquarestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.threelinesquarestepview.ThreeLineSquareStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ThreeLineSquareStepView.create(this)
     }
 }
