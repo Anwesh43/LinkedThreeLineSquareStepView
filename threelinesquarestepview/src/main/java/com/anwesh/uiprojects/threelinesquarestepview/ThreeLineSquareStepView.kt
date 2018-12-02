69f4579917372ed16341dc2d77edf576a9a4c3a1
Anwesh43 <anweshthecool0@gmail.com>
2018-12-02 18:42:36 +0530

    adding ThreeLineSquareStepView class

--- threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
+++ threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
@@ -62,3 +62,21 @@ fun Canvas.drawTLSSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ThreeLineSquareStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
