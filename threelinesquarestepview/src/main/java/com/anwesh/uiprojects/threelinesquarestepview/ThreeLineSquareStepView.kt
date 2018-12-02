1f7764fe7fa35a98577cb3b05b067273d70bcbd2
Anwesh43 <anweshthecool0@gmail.com>
2018-12-02 12:31:51 +0530

    adding libraries and defining constants

--- threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
+++ threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.threelinesquarestepview
 /**
  * Created by anweshmishra on 02/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val color : Int = Color.parseColor("#0D47A1")
+val sizeFactor : Int = 3
+val strokeFactor : Int = 120
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
