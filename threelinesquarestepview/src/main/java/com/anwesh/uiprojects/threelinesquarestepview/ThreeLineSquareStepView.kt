45e6c7863cfe667e625ef08bcde592ebabdf6819
Anwesh43 <anweshthecool0@gmail.com>
2018-12-02 19:17:06 +0530

    adding ThreeLineSquareStep class

--- threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
+++ threelinesquarestepview/src/main/java/com/anwesh/uiprojects/threelinesquarestepview/ThreeLineSquareStepView.kt
@@ -171,4 +171,27 @@ class ThreeLineSquareStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ThreeLineSquareStep(var i : Int) {
+        private val root : TLSSNode = TLSSNode(0)
+        private var curr : TLSSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
